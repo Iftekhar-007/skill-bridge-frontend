0f8dac21ce4b6b00ba035176ef095a418d563bfb
Iftekhar-007 <shawonsp534@gmail.com>
2026-02-24 14:13:04 +0600

    okay

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /* The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:5000",
+  baseURL: "https://skill-bridge-backend-pi.vercel.app",
 
   user: {
     additionalFields: {
@@ -11,23 +11,3 @@ export const authClient = createAuthClient({
     },
   },
 });
-
-// import { createAuthClient } from "better-auth/react";
-
-// type AdditionalUserFields = {
-//   role: "STUDENT" | "TUTOR";
-// };
-
-// export const authClient = createAuthClient<{
-//   user: AdditionalUserFields;
-// }>({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
-
-//   user: {
-//     additionalFields: {
-//       role: {
-//         type: "string",
-//       },
-//     },
-//   },
-// });
